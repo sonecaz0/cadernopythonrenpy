084a3a6ccfa278be1df91808664e66211b422f93
sonecaz <160507585+sonecaz0@users.noreply.github.com>
2024-08-27 11:30:43 -0300

    Add files via upload

---
+++ ola.py
@@ -0,0 +1,23 @@
+ola="ola mundo"
+print(ola)
+
+"""
+
+criando duas variaveis
+
+"""
+
+a=8
+b=12
+somar=a+b
+print("a soma é:",somar)
+
+sub=a-b
+print("a subtração é:",sub)
+
+mult=a*b
+print("a multiplicação é:",mult)
+
+div=a/b
+print("a dvisão é:",div)
+
