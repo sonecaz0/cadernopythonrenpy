b9f66c9caf4e9fb83288ddbe5d7449caa4fa1c4a
sonecaz <160507585+sonecaz0@users.noreply.github.com>
2024-09-03 10:44:41 -0300

    Add files via upload

---
+++ testes.py/script2.py
@@ -0,0 +1,27 @@
+# Atividade 2 Python
+
+""""
+Programador: Soneca
+
+Programa para testar varias funções em python
+
+"""
+if 10>1:
+    print("Condição VERDADEIRA")
+else:
+    print("Condição FALSA")
+print("fim do programa")
+
+x=str(3) # variavel tipo texto str
+y=int(3) # variavel tipo inteiro int
+z=float(3) # variavel tipo ponto flutuante float
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+frutas = ["banana", "maçã","mamão"]
+a, b, c = frutas
+print(a)
+print(b)
+print(c)
